4bd27fca4c8d03ad191f467b4064b38d03a8c66a
bouhvli <bouhali.hamza.2@gmail.com>
2023-02-21 03:00:43 -0800

    1st try

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+/**
+ * putchar0 - prints '_putchar'
+ * Description: Write a program that prints _putchar,
+ * followed by a new line.
+ * Return: 0 that mean success
+ */
+#include <main.h>
+
+int putchar0(void)
+{
+	char ch[] = "_putchar";
+	int i = 0;
+
+	while (ch)
+	{
+		_putchar(ch[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/template.c
@@ -0,0 +1,15 @@
+/**
+ * op_add - Makes the sum of two numbers
+ * @arg1: First operand
+ * @arg2: Second operand
+ *
+ * Description: This is a longer description.
+ * Don't forget that a line should not exceed 80 characters.
+ * But you're totally free to use several lines to properly
+ * describe your function
+ * Return: The sum of the two parameters
+ */
+int op_add(int arg1, int arg2)
+{
+	return (arg1 + arg2);
+}
