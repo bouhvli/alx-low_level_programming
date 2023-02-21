54487fcd34cc8ff2c32310311df6109ae2935e35
bouhvli <bouhali.hamza.2@gmail.com>
2023-02-21 03:11:30 -0800

    new changes

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,1 @@
-#ifndef _SAMPLE_HEADER_H_
-#define _SAMPLE_HEADER_H_
-
-/*
- * Structs, enums and unions definitions
- * Typedefs
- * Function prototypes
- */
 int _putchar(char c);
-
-#endif /* _SAMPLE_HEADER_H_ */
