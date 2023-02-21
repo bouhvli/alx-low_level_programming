2d481cc6bce3d3e9c8691a900d57a7a866fbb200
Hamza BOUHALI <79701412+bouhvli@users.noreply.github.com>
2023-02-21 18:20:40 +0100

    Create 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+/**
+ * print_to_98 - program that computes and prints the sum of all the
+ * multiples of 3 or 5 below 1024.
+ *
+ * Description:If we list all the natural numbers below 10
+ * that are multiples of 3 or 5,
+ * we get 3, 5, 6 and 9. The sum of these multiples is 23
+ * Return: 0.
+ */
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+  int number = 1024, i, sum = 0;
+
+  for(i = 0; i < number; i++)
+  {
+    if((i % 3) || (i % 5))
+    {
+      sum = sum + i;
+    }
+  }
+  printf("%d", sum);
+  return (0);
+}
