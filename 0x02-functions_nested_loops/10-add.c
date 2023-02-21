6c5480fd9cf11071d15e2a7ed5c233c9e6e865a4
bouhvli <bouhali.hamza.2@gmail.com>
2023-02-21 08:40:02 -0800

    lite

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+/**
+ * add - function that adds two integers and returns the result.
+ * @n: First operand
+ * @m: second operand
+ *
+ * Description: function that adds two integers and returns the result.
+ * Return: the sum of two values.
+ */
+#include "main.h"
+
+int add(int n, int m)
+{
+	return (n + m);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+/**
+ * print_to_98 - function that prints all natural numbers from n to 98.
+ * @n: First operand
+ *
+ * Description: Numbers must be separated by a comma, followed by a space
+ * Numbers should be printed in order
+ * The first printed number should be the number passed to your function
+ * The last printed number should be 98
+ * You are allowed to use the standard library
+ * Return: the sum of two values.
+ */
+#include "main.h"
+
+void print_to_98(int n)
+{
+	while (n > 97)
+	{
+		_putchar(n + '0');
+		_putchar(44);
+		_putchar(32);
+		n--;
+	}
+	while (n < 99)
+	{
+		_putchar(n + '0');
+		_putchar(44);
+		_putchar(32);
+		n++;
+	}
+	if (n == 98)
+		_putchar(n + '0');
+}
