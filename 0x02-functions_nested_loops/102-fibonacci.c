ea18fe704075389e313cd0f3bf47a3d768489441
Hamza BOUHALI <79701412+bouhvli@users.noreply.github.com>
2023-02-21 18:57:01 +0100

    Create 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+/**
+  * main - Prints the first 52 fibonacci numbers
+  *
+  * Return: Nothing!
+  */
+#include <stdio.h>
+
+int main(void)
+{
+	int i = 0;
+	long a = 1, b = 2;
+
+	while (i < 50)
+	{
+		if (i == 0)
+			printf("%ld", a);
+		else if (i == 1)
+			printf(", %ld", b);
+		else
+		{
+			b += a;
+			j = b - a;
+			printf(", %ld", b);
+		}
+
+		++i;
+	}
+
+	printf("\n");
+	return (0);
+}
