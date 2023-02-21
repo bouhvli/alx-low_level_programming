03e8b0e1c45f5b29a8317b07c8376cd84edca902
Hamza BOUHALI <79701412+bouhvli@users.noreply.github.com>
2023-02-21 18:33:12 +0100

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,5 +1,5 @@
 /**
- * print_to_98 - program that computes and prints the sum of all the
+ * main - program that computes and prints the sum of all the
  * multiples of 3 or 5 below 1024.
  *
  * Description:If we list all the natural numbers below 10
