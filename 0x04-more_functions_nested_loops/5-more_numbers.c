e187c1c4a3ed37a025bc283ac23abab320fd36af
bouhvli <bouhali.hamza.2@gmail.com>
2023-02-23 05:11:25 -0800

    change the for loop

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,14 +11,11 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 0; j < 15; j++)
 		{
-			_putchar(j + '0');
-		}
-		for (k = 0; k < 5; k++)
-		{
-			_putchar('1');
-			_putchar(k + '0');
+			if (j > 9)
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
 		}
 		_putchar('\n');
 	}
