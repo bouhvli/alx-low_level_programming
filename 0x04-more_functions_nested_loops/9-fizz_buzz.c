dc3dae27e83b7fba5c28b5cb3dd712be072c7ce2
bouhvli <bouhali.hamza.2@gmail.com>
2023-02-23 07:05:03 -0800

    deleting some white spaces

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,13 +7,14 @@
 int main(void)
 {
 	int i = 1;
+
 	while (i < 101)
-	{   
+	{
 		if ((i % 3 == 0) && (i % 5 == 0))
 		{
 			printf("FizzBuzz");
 		}
-		else if(i % 3 == 0)
+		else if (i % 3 == 0)
 		{
 		printf("Fizz");
 		}
@@ -28,5 +29,6 @@ int main(void)
 		printf(" ");
 		i++;
 	}
+	printf("\n");
 	return (0);
 }
