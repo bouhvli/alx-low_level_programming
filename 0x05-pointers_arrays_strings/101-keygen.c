c797184bafc927699f1374600f639de876cc5f5b
bouhvli <bouhali.hamza.2@gmail.com>
2023-02-27 07:58:20 -0800

    101 task

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+int main()
+{
+	int length = 16;
+	char *password = malloc(length + 1);
+
+	char *upper = "ABCDEFGHIJKLMNOPQRSTVWUXYZ";
+	char *lower = "abcdefghijklmnopqrstvwuxyz";
+	char *num = "1234567890";
+	char *symbol = "<>,.-_:;´¨{}[]^`+*¿?¡'!·$%&/()=@#¬";
+
+	int upper_length = strlen(upper);
+	int lower_length = strlen(lower);
+	int num_length = strlen(num);
+	int symbol_length = strlen(symbol);
+
+	srand(time(NULL) * getpid());
+
+	for (int i = 0; i < length; i++)
+	{
+		int c_type = rand() % 4;
+		if (c_type == 0)
+			password[i] = num[rand() % num_length];
+		else if (c_type == 1)
+			password[i] = lower[rand() % lower_length];
+		else if (c_type == 2)
+			password[i] = upper[rand() % upper_length];
+		else
+			password[i] = symbol[rand() % symbol_length];
+	}
+	password[length] = '\0';
+	printf("Tada! Congrats");
+	free(password);
+	return (0);
+}
