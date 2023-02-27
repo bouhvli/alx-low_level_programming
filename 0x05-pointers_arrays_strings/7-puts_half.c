ccb7ed6bb8dcf2c6c6ff89fc6d381728e9145b29
bouhvli <bouhali.hamza.2@gmail.com>
2023-02-27 06:24:58 -0800

    pushing task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * puts_half - prints half of a string
+ * @str: the string
+ *
+ * Description: prints a string,in reverse,
+ * followed by a new line,
+ * to stdout.
+ */
+void puts_half(char *str)
+{
+	int i, count = 0;
+
+	while (*(str + count) != '\0')
+	{
+		count++;
+	}
+	for (i = (count - 1) / 2; i < count - 1; i++)
+	{
+		if (i == (count - 1) / 2)
+			printf(", ");
+		_putchar(*(str + i));
+	}
+	_putchar('\n');
+}
