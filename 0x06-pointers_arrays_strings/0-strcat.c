499eb7ba126648585d03bfba29f308bde7b1a96b
bouhvli <bouhali.hamza.2@gmail.com>
2023-03-01 03:09:11 -0800

    cleaning the code with the help of betty

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,15 +5,16 @@
  * @src: second string
  *
  * Description: a function that concatenates two string.
+ * Return: concatenated string.
  */
 char *_strcat(char *dest, char *src)
 {
 	int i = 0, j = 0;
 
 	while (*(dest + i) != '\0')
-	{	
+	{
 		i++;
-		if (*(dest + i) == '\0' )
+		if (*(dest + i) == '\0')
 		{
 			while (*(src + j) != '\0')
 			{
@@ -23,5 +24,5 @@ char *_strcat(char *dest, char *src)
 			}
 		}
 	}
-	return dest;
+	return (dest);
 }
