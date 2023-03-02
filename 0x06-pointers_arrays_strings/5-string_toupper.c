7e8c7ae479ac747aa0a38c41e13dce31f16ca384
bouhvli <bouhali.hamza.2@gmail.com>
2023-03-02 00:14:36 -0800

    new changes

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * reverse_array - function that recieve a string and number
+ * @str: first string
+ *
+ * Description: a function that changes all lowercase.
+ * letters of a string to uppercase
+ * Return: string
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] > 96 && str[i] < 123)
+			str[i] -= 32;
+	}
+	return (str)
+}
