a3c90fab23ad21d1492db94ad694c2076042447b
bouhvli <bouhali.hamza.2@gmail.com>
2023-03-02 00:41:51 -0800

    pushing task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -13,15 +13,15 @@ char *leet(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] == 'a' || str[i] == 'A')
+		while (str[i] == 'a' || str[i] == 'A')
 			str[i] = '4';
-		else if (str[i] == 'e' || str[i] == 'E')
+		while (str[i] == 'e' || str[i] == 'E')
 			str[i] = '3';
-		else if (str[i] == 'o' || str[i] == 'O')
+		while (str[i] == 'o' || str[i] == 'O')
 			str[i] = '0';
-		else if (str[i] == 't' || str[i] == 'T')
+		while (str[i] == 't' || str[i] == 'T')
 			str[i] = '7';
-		else if (str[i] == 'l' || str[i] == 'L')
+		while (str[i] == 'l' || str[i] == 'L')
 			str[i] = '1';
 	}
 
