33e67c520e4c95e2e7f5efb577cc2e9b24c955b4
bouhvli <bouhali.hamza.2@gmail.com>
2023-03-02 00:40:14 -0800

    pushing task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * leet - function that recieve a string.
+ * @str: first string
+ *
+ * Description: a function that encodes a
+ * string into 1337.
+ * Return: string
+ */
+char *leet(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == 'a' || str[i] == 'A')
+			str[i] = '4';
+		else if (str[i] == 'e' || str[i] == 'E')
+			str[i] = '3';
+		else if (str[i] == 'o' || str[i] == 'O')
+			str[i] = '0';
+		else if (str[i] == 't' || str[i] == 'T')
+			str[i] = '7';
+		else if (str[i] == 'l' || str[i] == 'L')
+			str[i] = '1';
+	}
+
+	return (str);
+}
