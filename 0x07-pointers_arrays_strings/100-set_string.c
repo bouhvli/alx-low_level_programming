d99c4a0a30d71e00280daf1e7616e827edfd0958
bouhvli <bouhali.hamza.2@gmail.com>
2023-03-06 09:37:09 -0800

    pushing task 9

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+/**
+ * print_diagsums - a function that sets the value of a
+ * pointer to a char.
+ * @s: the address of the string
+ * @to:the address of the string
+ *
+ */
+#include "main.h"
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
