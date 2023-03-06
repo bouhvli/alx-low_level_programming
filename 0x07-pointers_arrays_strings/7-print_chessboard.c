7458ab1454931f390f31003f339be5dbd1f3a705
Hamza BOUHALI <79701412+bouhvli@users.noreply.github.com>
2023-03-06 18:16:35 +0100

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,15 +2,8 @@
  * print_chessboard - a function that prints the chessboard.
  * @a: the address of the string
  *
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
  */
 #include "main.h"
-
-int _putchar(char c);
 
 void print_chessboard(char (*a)[8])
 {
