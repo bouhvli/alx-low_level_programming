ca41079bb5e9a53918cfc60f9b189c9169d5fa2d
bouhvli <bouhali.hamza.2@gmail.com>
2023-03-06 06:03:48 -0800

    pushing task after gcc, and betty cheking

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+/**
+ * _strchr - prints buffer in hexa
+ * @s: the address of the string
+ * @c: the char we look for in the string
+ *
+ * Return: the pointer to the position of the char
+ * if matched or NULL if not.
+ */
+#include "main.h"
+
+char *_strchr(char *s, char c)
+{
+	char *ptr;
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) == c)
+		{
+			ptr = (s + i);
+			return (ptr);
+		}
+		i++;
+	}
+	return ('\0');
+}
