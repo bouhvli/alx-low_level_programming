d8df69b0cb058414adfba2f4a6c9495ed2d5c407
bouhvli <bouhali.hamza.2@gmail.com>
2023-03-06 09:10:15 -0800

    pushing task 6

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+/**
+ * print_chessboard - a function that prints the chessboard.
+ * @a: the address of the string
+ *
+ */
+#include "main.h"
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			if (j == 7)
+			{
+				_putchar(a[i][j]);
+				_putchar('\n');
+			}
+			else
+				_putchar(a[i][j]);
+		}
+	}
+}
