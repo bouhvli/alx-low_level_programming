c8bc233adf341c2528e6b7aaa357ebcc5ef73832
bouhvli <bouhali.hamza.2@gmail.com>
2023-03-06 05:25:40 -0800

    first commit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+/**
+ *  _memset - takes a string and a value and number of bytes.
+ *  @s: the string to alter.
+ *  @b: the new value.
+ *  @n: the number of bytes.
+ *
+ *  Return: the string altered.
+ */
+#include "main.h"
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b;
+	}
+	return (s);
+}
