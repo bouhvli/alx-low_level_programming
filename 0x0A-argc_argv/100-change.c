3dd1f6e3120ea321a50330697a45fb1d05f0306b
Hamza BOUHALI <79701412+bouhvli@users.noreply.github.com>
2023-03-10 21:49:08 +0100

    Update 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -6,30 +6,6 @@
 * @argv: array of pointers to arguement strings
 * Return: number of coins or 1
 **/
-/**
-int main(int argc, char *argv[])
-{
-int amount = atoi(argv[1]);
-int coins = 0;
-int denominations[] = {25, 10, 5, 2, 1};
-int i = 0;
-int n = sizeof(denominations) / sizeof(denominations[0]);
-
-	if (argc != 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	while (amount > 0 && i < n)
-	{
-		coins += amount / denominations[i];
-		amount %= denominations[i];
-		i++;
-	}
-	printf("%d\n", coins);
-	return (0);
-}
-*/
 int main(int argc, char *argv[])
 {
 	int amount, coins;
