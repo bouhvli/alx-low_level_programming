993d19d80a4158830a3aa960a1e1c3a16f4cf743
bouhvli <bouhali.hamza.2@gmail.com>
2023-03-10 12:45:21 -0800

    my blood in here

--- 0x0A-argc_argv/100.c
+++ 0x0A-argc_argv/100.c
@@ -1,14 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-/**
- * main - prints the minimum number of coins to make
- * change for an amount of money.
- * @argc: the number of args
- * @argv: the args entered 'string format'
- *
- * Return: if the number of arguments passed to your program is
- * not exactly 1, print Error, followed by a new line, and return 1
- */
+
 int main(int argc, char *argv[])
 {
 int amount = atoi(argv[1]);
@@ -20,15 +12,15 @@ int n = sizeof(denominations) / sizeof(denominations[0]);
 	if (argc != 2)
 	{
 		printf("Error\n");
-		return (1);
+		return 1;
 	}
 	while (amount > 0 && i < n)
 	{
 		coins += amount / denominations[i];
 		amount %= denominations[i];
 		i++;
-	}
+	}   
 	printf("%d\n", coins);
-	return (0);
+	return 0;
 }
 
