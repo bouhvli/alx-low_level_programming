275a7c897b3ce7a7e6ada8408924d07d62e5f84d
bouhvli <bouhali.hamza.2@gmail.com>
2023-03-10 10:40:09 -0800

    adding task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+/**
+ * main - show the number of args
+ * @argc: the number of args
+ * @argv: the args entered 'string format'
+ *
+ * Return: 0
+ */
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+	_putchar(argc + '0');
+	_putchar('\n');
+	return (0);
+}
