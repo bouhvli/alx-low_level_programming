62620ea568680984499a431be3ec6837883c5526
Hamza BOUHALI <79701412+bouhvli@users.noreply.github.com>
2023-03-10 20:38:28 +0100

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,28 +1,14 @@
 /**
- * main - a program that multiplies two numbers.
- * @argc: number
+ * _atio_multi - a program that multiplies two numbers.
  * @argv: name of args
  *
- * Return:If the program does not receive two arguments,
- * your program should print Error, followed by a new line, and return 1.
+ * Return:result.
  */
-#include "main.h"
-int main(int argc, char *argv[]) 
+int _atio_multi(char *argv[])
 {
 	int num1 = 0;
 	int num2 = 0;
-	int result, i;
-	i = 0;
-	if (argc != 3) 
-	{
-		_putchar('E');
-		_putchar('r');
-		_putchar('r');
-		_putchar('o');
-		_putchar('r');
-		_putchar('\n');
-		return 1;
-	}
+	
 	while (argv[1][i] != '\0')
         {
                 num1 = num1 * 10 + (argv[1][i] - '0');
@@ -34,17 +20,39 @@ int main(int argc, char *argv[])
                 num2 = num2 * 10 + (argv[2][i] - '0');
                 i++;
         }
-        result = num1 * num2;
-	if (result < 0) 
+	return (num1 * num2);
+}
+/**
+ * main - a program that give the result of two numbers multiplied.
+ * @argc: number
+ * @argv: name of args
+ *
+ * Return:If the program does not receive two arguments,
+ * your program should print Error, followed by a new line, and return 1.
+ */
+#include "main.h"
+int main(int argc, char *argv[])
+{
+
+	int result, i;
+	i = 0;
+	
+	if (argc != 3)
 	{
-		_putchar('-');
-		result = -result;
+		_putchar('E');
+		_putchar('r');
+		_putchar('r');
+		_putchar('o');
+		_putchar('r');
+		_putchar('\n');
+		return (1);
 	}
-	if (result == 0) 
+        result = _atio_multi(argv);
+	if (result == 0)
 	{
 		_putchar('0');
 	}
-	while (result > 0) 
+	while (result > 0)
 	{
 		int digit = result % 10;
 		_putchar(digit + '0');
