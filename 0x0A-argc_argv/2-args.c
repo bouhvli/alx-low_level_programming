a352353baa5775cfd0ced3546c230f24163964c9
bouhvli <bouhali.hamza.2@gmail.com>
2023-03-10 10:53:34 -0800

    update task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,26 @@
+/**
+ * main - the main function
+ * @argc: count number of args
+ * @argv: store the name of it
+ *
+ * Return: 0
+ */
+#include "main.h"
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (i < argc)
+	{
+		char *arg = argv[i];
+
+		while (*arg != '\0')
+		{
+			_putchar(*arg);
+			arg++;
+		}
+		_putchar('\n');
+		i++;
+	}
+	return (0);
+}
