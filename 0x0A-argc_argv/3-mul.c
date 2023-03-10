57e473e58d34e1911b7f39e25383c8645dd34c61
bouhvli <bouhali.hamza.2@gmail.com>
2023-03-10 11:45:22 -0800

    update 3 times 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,30 @@
 /**
- * main - a program that multiplies two numbers.
+ * _atio_multi - a program that multiplies two numbers.
+ * @argv: name of args
+ *
+ * Return:result.
+ */
+int _atio_multi(char *argv[])
+{
+	int num1 = 0;
+	int num2 = 0;
+	int i = 0;
+	
+	while (argv[1][i] != '\0')
+        {
+                num1 = num1 * 10 + (argv[1][i] - '0');
+                i++;
+        }
+        i = 0;
+        while (argv[2][i] != '\0')
+        {
+                num2 = num2 * 10 + (argv[2][i] - '0');
+                i++;
+        }
+	return (num1 * num2);
+}
+/**
+ * main - a program that give the result of two numbers multiplied.
  * @argc: number
  * @argv: name of args
  *
@@ -7,9 +32,12 @@
  * your program should print Error, followed by a new line, and return 1.
  */
 #include "main.h"
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[])
 {
-	if (argc != 3) 
+
+	int result;
+	
+	if (argc != 3)
 	{
 		_putchar('E');
 		_putchar('r');
@@ -17,33 +45,14 @@ int main(int argc, char *argv[])
 		_putchar('o');
 		_putchar('r');
 		_putchar('\n');
-		return 1;
+		return (1);
 	}
-
-	int num1 = 0;
-	int num2 = 0;
-
-	for (int i = 0; argv[1][i] != '\0'; i++) 
-	{
-		num1 = num1 * 10 + (argv[1][i] - '0');
-	}
-	for (int i = 0; argv[2][i] != '\0'; i++) 
-	{
-		num2 = num2 * 10 + (argv[2][i] - '0');
-	}
-
-	int result = num1 * num2;
-
-	if (result < 0) 
-	{
-		_putchar('-');
-		result = -result;
-	}
-	if (result == 0) 
+        result = _atio_multi(argv);
+	if (result == 0)
 	{
 		_putchar('0');
 	}
-	while (result > 0) 
+	while (result > 0)
 	{
 		int digit = result % 10;
 		_putchar(digit + '0');
@@ -53,3 +62,4 @@ int main(int argc, char *argv[])
 
 	return (0);
 }
+
--- 0x0A-argc_argv/test.c
+++ 0x0A-argc_argv/test.c
@@ -1,44 +1,56 @@
-#include <stdio.h>
-int main(int argc, char *argv[]) {
-    // Check if the program received two arguments
-    if (argc != 3) {
-        // Print an error message and return 1
-        putchar('E');
-        putchar('r');
-        putchar('r');
-        putchar('o');
-        putchar('r');
-        putchar('\n');
-        return 1;
-    }
+/**
+ * main - a program that multiplies two numbers.
+ * @argc: number
+ * @argv: name of args
+ *
+ * Return:If the program does not receive two arguments,
+ * your program should print Error, followed by a new line, and return 1.
+ */
+#include "main.h"
+int main(int argc, char *argv[]) 
+{
+	int num1 = 0;
+        int num2 = 0;
+        int i = 0;
+	int result;
 
-    // Convert the arguments to integers
-    int num1 = 0;
-    int num2 = 0;
-    for (int i = 0; argv[1][i] != '\0'; i++) {
-        num1 = num1 * 10 + (argv[1][i] - '0');
-    }
-    for (int i = 0; argv[2][i] != '\0'; i++) {
-        num2 = num2 * 10 + (argv[2][i] - '0');
-    }
+	if (argc != 3) 
+	{
+		putchar('E');
+		putchar('r');
+		putchar('r');
+		putchar('o');
+		putchar('r');
+		putchar('\n');
+		return (1);
+	}
+	while (argv[1][i] != '\0') 
+	{
+		num1 = num1 * 10 + (argv[1][i] - '0');
+		i++;
+	}
+	i = 0;
+	while (argv[2][i] != '\0')
+	{
+		num2 = num2 * 10 + (argv[2][i] - '0');
+		i++;
+	}
+	result = num1 * num2;
+	if (result < 0) 
+	{
+		_putchar('-');
+		result = -result;
+	}
+	if (result == 0) 
+	{
+		_putchar('0');
+	}
+	while (result > 0) 
+	{
+		int digit = result % 10;
+		_putchar(digit + '0');
+		result /= 10;
+	}
+_putchar('\n');
 
-    // Multiply the two numbers
-    int result = num1 * num2;
-
-    // Print the result followed by a newline
-    if (result < 0) {
-        putchar('-');
-        result = -result;
-    }
-    if (result == 0) {
-        putchar('0');
-    }
-    while (result > 0) {
-        int digit = result % 10;
-        putchar(digit + '0');
-        result /= 10;
-    }
-    putchar('\n');
-
-    return 0;
-}
+	return }
