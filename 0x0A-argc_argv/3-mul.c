f6bd8eb2aa6bc75d2f71524fe5b3ab5334278494
bouhvli <bouhali.hamza.2@gmail.com>
2023-03-10 12:03:21 -0800

    gh

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,25 +1,72 @@
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
-* main - multiply two numbers
-* @argc: arguement count
-* @argv: array of pointers to arguement strings
-* Return: result of multiplication or 1
-**/
+ * _atio_multi - a program that multiplies two numbers.
+ * @argv: name of args
+ *
+ * Return:result.
+ */
+int _atio_multi(char *argv[])
+{
+	int num1 = 0;
+	int num2 = 0;
+	int i = 0;
+
+	while (argv[1][i] != '\0')
+	{
+		num1 = num1 * 10 + (argv[1][i] - '0');
+		i++;
+	}
+	i = 0;
+	while (argv[2][i] != '\0')
+	{
+		num2 = num2 * 10 + (argv[2][i] - '0');
+		i++;
+	}
+	return (num1 * num2);
+}
+/**
+ * main - a program that give the result of two numbers multiplied.
+ * @argc: number
+ * @argv: name of args
+ *
+ * Return:If the program does not receive two arguments,
+ * your program should print Error, followed by a new line, and return 1.
+ */
+#include "main.h"
 int main(int argc, char *argv[])
 {
-	int a, b, result;
+	int result;
 
-	if (argc <= 2)
+	if (argc != 3)
 	{
-		printf("Error\n");
+		_putchar('E');
+		_putchar('r');
+		_putchar('r');
+		_putchar('o');
+		_putchar('r');
+		_putchar('\n');
 		return (1);
 	}
-/* atoi convert a string to an int */
-	a = atoi(argv[1]);
-	b = atoi(argv[2]);
-	result = a * b;
-	printf("%d\n", result);
+
+	result = _atio_multi(argv);
+	
+	if (result < 0)
+	{
+		putchar('-');
+		result = -result;
+    	}
+	if (result == 0)
+	{
+		_putchar('0');
+	}
+	while (result > 0)
+	{
+		int digit = result % 10;
+
+		_putchar(digit + '0');
+		result /= 10;
+	}
+	_putchar('\n');
+
 	return (0);
 }
+
