008f3a808351240ea2015b6b612850c2d6e2b423
bouhvli <bouhali.hamza.2@gmail.com>
2023-03-13 05:43:01 -0700

    pushing task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * len - help to get the length of the string passed to
+ * parametars
+ * @str: the string we want to calc it's length.
+ *
+ * Return: the length.
+ */
+int len(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+ * _strdup - returns a pointer to a newly allocated space
+ * in memory, which contains a copy of the string given as
+ * a parameter.
+ * @str: string we want to copy.
+ *
+ * Return: a pointer to the copyed string.
+ */
+char *_strdup(char *str)
+{
+	char *ptrs;
+	int i = 0, length = len(str);
+
+	_putchar(length + '0');
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		ptrs = malloc(sizeof(char) * length);
+		if (ptrs == NULL)
+		{
+			return (NULL);
+		}
+		else
+		{
+			while (*(str + i) != '\0')
+			{
+				*(ptrs + i) = *(str + i);
+				i++;
+			}
+		}
+		return (ptrs);
+	}
+}
