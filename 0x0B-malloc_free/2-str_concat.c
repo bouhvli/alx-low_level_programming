0d5536884ba1483c3ac2b61c0779088d66f6cb91
bouhvli <bouhali.hamza.2@gmail.com>
2023-03-13 07:44:03 -0700

    update 0.2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -45,13 +45,11 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 	{
-		*(s1 + 0) = ' ';
-		*(s1 + 1) = '\0';
+		*(s1 + 0) = '\0';
 	}
 	else if (s2 == NULL)
 	{
-		*(s2 + 0) = ' ';
-		*(s2 + 1) = '\0';
+		*(s2 + 0) = '\0';
 	}
 	else
 	{
