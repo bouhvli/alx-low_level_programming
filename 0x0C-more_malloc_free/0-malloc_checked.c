1979927f7c572a947f528e5853aeef2bf4545b07
bouhvli <bouhali.hamza.2@gmail.com>
2023-03-16 02:58:15 -0700

    update declaration of the returned pointer

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -14,12 +14,9 @@ void *malloc_checked(unsigned int b)
 {
 	void *ptr = malloc(b);
 
-	if (ptr != NULL)
+	if (ptr == NULL)
 	{
-		return (malloc(b));
+		exit(98)
 	}
-	else
-	{
-		exit(98);
-	}
+	return (malloc(b));
 }
