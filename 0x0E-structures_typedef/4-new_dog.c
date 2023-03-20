43bd81fdd7ad0eaab7a73fd2660ad5bbb91886ac
bouhvli <bouhali.hamza.2@gmail.com>
2023-03-20 07:12:54 -0700

    update the sltrln

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,16 +11,34 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	char *owner_copy;
+	char *name_copy;
 	struct dog *n_dog;
 
 	n_dog = malloc(sizeof(struct dog));
-	if (n_dog != NULL)
+	if (n_dog == NULL)
 	{
-		n_dog->name = name;
-		n_dog->age = age;
-		n_dog->owner = owner;
-		return (n_dog);
-	}
-	else
 		return (NULL);
+	}
+	name_copy = malloc(strlen(name) + 1);
+	if (name_copy == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	strcpy(name_copy, name);
+
+	owner_copy = malloc(strlen(owner) + 1);
+	if (owner_copy == NULL)
+	{
+		free(name_copy);
+		free(new_dog);
+		return (NULL);
+	}
+	strcpy(owner_copy, owner)
+	n_dog->name = name;
+	n_dog->age = age;
+	n_dog->owner = owner;
+	return (n_dog);
+
 }
