f4b6d9244d37f3239c10cf7a63a8f5a578ad6920
Hamza BOUHALI <79701412+bouhvli@users.noreply.github.com>
2023-03-20 15:39:47 +0000

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,60 +1,38 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "dog.h"
+#include <stdlib.h>
 /**
- * _strlen - count the number of bytes in the string
- * @s: the string
- *
- * Return: the number of bytes.
- */
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (*(s + i) != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
-/**
- * new_dog - create new dog
- * @name: of the dog.
- * @age: of the dog.
- * @owner: of the dog.
- * Return: null if n_dog is null, and the pointer to the
- * new dog :-) if the everything is okee.
- */
+  * new_dog - creates a new struct of type dog
+  * @name: struct parameter name
+  * @age: struct parameter age
+  * @owner: struct parameter owner
+  * Return: returns pointer to buffer of datatype dog
+  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	char *owner_copy;
-	char *name_copy;
-	dog_t *n_dog;
+int nlen, olen, i;
+dog_t *n_dog;
 
-	n_dog = malloc(sizeof(dog_t));
-	if (n_dog == NULL)
-	{
-		return (NULL);
-	}
-	name_copy = malloc(_strlen(name) + 1);
-	if (name_copy == NULL)
-	{
-		free(n_dog);
-		return (NULL);
-	}
-	name_copy = name;
+nlen = olen = 0;
+while (name[nlen++])
+;
+while (owner[olen++])
+;
+n_dog = malloc(sizeof(dog_t));
+if (n_dog == NULL)
+return (NULL);
 
-	owner_copy = malloc(_strlen(owner) + 1);
-	if (owner_copy == NULL)
-	{
-		free(name_copy);
-		free(n_dog);
-		return (NULL);
-	}
-	owner_copy = owner;
-	n_dog->name = name_copy;
-	n_dog->age = age;
-	n_dog->owner = owner_copy;
-	return (n_dog);
+n_dog->name = malloc(nlen * sizeof(n_dog->name));
+if (n_dog == NULL)
+return (NULL);
+for (i = 0; i < nlen; i++)
+n_dog->name[i] = name[i];
 
+n_dog->age = age;
+
+n_dog->owner = malloc(olen * sizeof(n_dog->owner));
+if (n_dog == NULL)
+return (NULL);
+for (i = 0; i < olen; i++)
+n_dog->owner[i] = owner[i];
+return (n_dog);
 }
