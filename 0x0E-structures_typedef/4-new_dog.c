cd55717a7164fc4704adf228f9ce188393a5fa28
Hamza BOUHALI <79701412+bouhvli@users.noreply.github.com>
2023-03-20 14:48:39 +0000

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -29,9 +29,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	char *owner_copy;
 	char *name_copy;
-	struct dog *n_dog;
+	dog_t *n_dog;
 
-	n_dog = malloc(sizeof(struct dog));
+	n_dog = malloc(sizeof(dog_t));
 	if (n_dog == NULL)
 	{
 		return (NULL);
