9a4568e533c532c6d8e410b200935fa1b4d247a8
bouhvli <bouhali.hamza.2@gmail.com>
2023-03-20 08:43:04 -0700

    last update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,38 +1,38 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
-  * new_dog - creates a new struct of type dog
-  * @name: struct parameter name
-  * @age: struct parameter age
-  * @owner: struct parameter owner
-  * Return: returns pointer to buffer of datatype dog
-  */
+ * new_dog - creates a new struct of type dog
+ * @name: struct parameter name
+ * @age: struct parameter age
+ * @owner: struct parameter owner
+ * Return: returns pointer to buffer of datatype dog
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-int nlen, olen, i;
-dog_t *n_dog;
+	int nlen, olen, i;
+	dog_t *n_dog;
 
-nlen = olen = 0;
-while (name[nlen++])
-;
-while (owner[olen++])
-;
-n_dog = malloc(sizeof(dog_t));
-if (n_dog == NULL)
-return (NULL);
+	nlen = olen = 0;
+	while (name[nlen++])
+		;
+	while (owner[olen++])
+		;
+	n_dog = malloc(sizeof(dog_t));
+	if (n_dog == NULL)
+		return (NULL);
 
-n_dog->name = malloc(nlen * sizeof(n_dog->name));
-if (n_dog == NULL)
-return (NULL);
-for (i = 0; i < nlen; i++)
-n_dog->name[i] = name[i];
+	n_dog->name = malloc(nlen * sizeof(n_dog->name));
+	if (n_dog == NULL)
+		return (NULL);
+	for (i = 0; i < nlen; i++)
+		n_dog->name[i] = name[i];
 
-n_dog->age = age;
+	n_dog->age = age;
 
-n_dog->owner = malloc(olen * sizeof(n_dog->owner));
-if (n_dog == NULL)
-return (NULL);
-for (i = 0; i < olen; i++)
-n_dog->owner[i] = owner[i];
-return (n_dog);
+	n_dog->owner = malloc(olen * sizeof(n_dog->owner));
+	if (n_dog == NULL)
+		return (NULL);
+	for (i = 0; i < olen; i++)
+		n_dog->owner[i] = owner[i];
+	return (n_dog);
 }
