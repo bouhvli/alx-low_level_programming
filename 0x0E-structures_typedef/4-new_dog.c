1454142f6f85faf84fef11db53ee3c6821759c68
bouhvli <bouhali.hamza.2@gmail.com>
2023-03-20 10:10:26 -0700

    adding free

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -20,18 +20,23 @@ dog_t *new_dog(char *name, float age, char *owner)
 	n_dog = malloc(sizeof(dog_t));
 	if (!n_dog)
 		return (NULL);
-
-	n_dog->name = malloc(nlen * sizeof(char) + 1);
+	n_dog->name = malloc(nlen + 1);
 	if (!n_dog->name)
+	{
+		free(n_dog);
 		return (NULL);
+	}
 	for (i = 0; i < nlen; i++)
 		n_dog->name[i] = name[i];
 	n_dog->name[i] = '\0';
 	n_dog->age = age;
-
-	n_dog->owner = malloc(olen * sizeof(char) + 1);
+	n_dog->owner = malloc(olen + 1);
 	if (!n_dog->owner)
+	{
+		free(n_dog->name);
+		free(n_dog);
 		return (NULL);
+	}
 	for (i = 0; i < olen; i++)
 		n_dog->owner[i] = owner[i];
 	n_dog->owner[i] = '\0';
