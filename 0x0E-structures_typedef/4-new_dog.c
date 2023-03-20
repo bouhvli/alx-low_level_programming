892e7c2c1ebf6e46fc08eee8a17cf2c99e7210f5
bouhvli <bouhali.hamza.2@gmail.com>
2023-03-20 06:18:12 -0700

    pushing task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * new_dog - create new dog
+ * @name: of the dog.
+ * @age: of the dog.
+ * @owner: of the dog.
+ *
+ * Return: null if n_dog is null, and the pointer to the new dog :-) if the
+ * everything is okee.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *n_dog = malloc(sizeof(dog_t));
+
+	if (n_dog == NULL)
+		return (NULL);
+
+	n_dog->name = name;
+	n_dog->age = age;
+	n_dog->owner = owner;
+	return (n_dog);
+}
