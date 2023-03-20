689ce975317283d7f676208e61c0885c301328e1
bouhvli <bouhali.hamza.2@gmail.com>
2023-03-20 04:19:42 -0700

    adding DOC_H to the dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,16 +1,18 @@
-#ifndef DOG
-#define DOG
+#ifndef DOG_H
+#define DOG_H
 
 /**
  * struct dog - Define a new type struct dog.
  * @name: representation of the name of the dog.
  * @age: the age of the dog
  * @owner: identifies the owner of the dog.
+ *
+ * Description: Define a new type struct dog.
  */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
 #endif
