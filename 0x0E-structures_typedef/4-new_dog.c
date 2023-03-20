56f1afed2c62d670903788b9baeef3a25d80f27a
bouhvli <bouhali.hamza.2@gmail.com>
2023-03-20 06:30:53 -0700

    new

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -7,14 +7,14 @@
  * @age: of the dog.
  * @owner: of the dog.
  *
- * Return: null if n_dog is null, and the pointer to the new dog :-) if the
- * everything is okee.
+ * Return: null if n_dog is null, and the pointer to the
+ * new dog :-) if the everything is okee.
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *n_dog;
 
-	n_dog = malloc(sizeof(dog_t *));
+	n_dog = malloc(sizeof(dog_t));
 	if (n_dog == NULL)
 		return (NULL);
 	n_dog->name = name;
