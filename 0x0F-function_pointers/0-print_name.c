381885847c136fe1ba92df9ff822f2327aac838e
bouhvli <bouhali.hamza.2@gmail.com>
2023-03-22 03:50:11 -0700

    looking for errors

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,6 +4,10 @@
  * @name: the name we need to print.
  * @f: the function we want to point to.
  *
+ * Description: the function taks two params one is
+ * string and the other is function pointer, that will
+ * call the function entered to param with name as param for that
+ * function to do the job.
  */
 void print_name(char *name, void (*f)(char *))
 {
