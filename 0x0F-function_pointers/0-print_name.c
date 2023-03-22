c695520e5536bfba4cc54a0c2d588bc10265f96a
bouhvli <bouhali.hamza.2@gmail.com>
2023-03-22 04:03:45 -0700

    update adding stdio.h

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "function_pointers.h"
+
 /**
  * print_name - a function that prints a name.
  * @name: the name we need to print.
@@ -10,6 +11,7 @@
  * call the function entered to param with name as param for that
  * function to do the job.
  */
+
 void print_name(char *name, void (*f)(char *))
 {
 	f(name);
