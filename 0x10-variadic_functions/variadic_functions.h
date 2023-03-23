24f32ecc7fa710b54e73d725c94814d8b65008d4
Hamza BOUHALI <79701412+bouhvli@users.noreply.github.com>
2023-03-23 14:33:36 +0000

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -7,4 +7,15 @@ void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 int _putchar(char c);
 
+/**
+ * struct print - a structer used to assign the func name to the equivalent type.
+ * @type: the type of the param.
+ * @func_print: function pointer to the fucntion that will print the value of the
+ * corespondent type.
+ */
+typedef struct print
+{
+char *type;
+void (*func_print)(va_list arg);
+} print;
 #endif
