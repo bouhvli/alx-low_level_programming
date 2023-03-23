9bc699d72a6e568f6ea190091d9484cb47d77b0d
bouhvli <bouhali.hamza.2@gmail.com>
2023-03-23 07:50:55 -0700

    all done

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,71 +3,94 @@
 #include <stdarg.h>
 #include "variadic_functions.h"
 /**
- * look_for - this is the core of the print_all function,
- * this function will find matches in the type and print it.
- * @a: the type of the param we want to print it value.
- * @args: the list of arguments intered to the print_all func.
- *
- * Return: 1 if there is an unknown type.
+ * output_c - print the char type.
+ * @arg: the args list.
  */
-int look_for(char a, va_list args)
+void output_c(va_list arg)
 {
-	int com;
 	char c;
-	int d;
-	double f;
+
+	c = va_arg(arg, int);
+	printf("%c", c);
+}
+
+/**
+ * output_i - print the int type.
+ * @arg: the args list.
+ */
+void output_i(va_list arg)
+{
+	int i;
+
+	i = va_arg(arg, int);
+	printf("%d", i);
+}
+
+/**
+ * output_f - print float type.
+ * @arg: the args list.
+ */
+void output_f(va_list arg)
+{
+	float f;
+
+	f = va_arg(arg, double);
+	printf("%f", f);
+}
+
+/**
+ * output_s - print the string type.
+ * @arg: the args list.
+ */
+void output_s(va_list arg)
+{
 	char *s;
 
-	com = 0;
-	switch (a)
+	s = va_arg(arg, char *);
+	if (s == NULL)
 	{
-		case 'c':
-			c = (char)va_arg(args, int);
-			printf("%c", c);
-			break;
-		case 'i':
-			d = va_arg(args, int);
-			printf("%d", d);
-			break;
-		case 'f':
-			f = va_arg(args, double);
-			printf("%f", f);
-			break;
-		case 's':
-			s = va_arg(args, char *);
-			if (s == NULL)
-				printf("(nil)");
-			else
-				printf("%s", s);
-			break;
-		default:
-			com = 1;
-			break;
+		printf("(nil)");
+		return;
 	}
-	return (com);
+	printf("%s", s);
 }
+
 /**
- * print_all - Prints a formatted string to the console, with variable
- * number of arguments.
- * @format: is a list of types of arguments passed to the function.
+ * print_all - Prints anything, followed by a new line.
+ * @format: the argument types.
+ *
+ * Description: Any argument not of type char, int, float,
+ * or char * is ignored.
+ * If a string is NULL print (nil) instead.
  */
-void print_all(const char *const format, ...)
+void print_all(const char * const format, ...)
 {
 	va_list args;
-	int i;
-	int com;
+	int i = 0, j = 0;
+	char *separator = "";
+	print type_func[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string}
+	};
 
-	i = 0;
-	com = 0;
 	va_start(args, format);
-	while (format[i] != '\0')
+	while (format && (*(format + i)))
 	{
-		com = look_for(format[i], args);
+		j = 0;
+		while (j < 4 && (*(format + i) != *(type_func[j].type)))
+			j++;
+
+		if (j < 4)
+		{
+			printf("%s", separator);
+			type_func[j].func_print(args);
+			separator = ", ";
+		}
 		i++;
-		if (format[i] != '\0' && com != 1)
-			printf(", ");
-		com = 0;
 	}
-	va_end(args);
 	printf("\n");
+	va_end(args);
 }
+
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,14 +8,16 @@ void print_all(const char * const format, ...);
 int _putchar(char c);
 
 /**
- * struct print - a structer used to assign the func name to the equivalent type.
+ * struct print - a structer used to assign the func name
+ * to the equivalent type.
+ *
  * @type: the type of the param.
- * @func_print: function pointer to the fucntion that will print the value of the
- * corespondent type.
+ * @func_print: function pointer to the fucntion that will
+ * print the value of the corespondent type.
  */
 typedef struct print
 {
-char *type;
-void (*func_print)(va_list arg);
+	char *type;
+	void (*func_print)(va_list arg);
 } print;
 #endif
