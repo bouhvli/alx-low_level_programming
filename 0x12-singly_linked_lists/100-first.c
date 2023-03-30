e435f61f0d0c2e5f61f642a70ab415620eea7e2f
bouhvli <bouhali.hamza.2@gmail.com>
2023-03-30 08:14:32 -0700

    pushing task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void before_main(void) __attribute__((constructor));
+/**
+ * before_main - a function that prints
+ * -You're beat! and yet, you must allow,\nI bore my house upon my back!\n-
+ * before the main function is executed.
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
