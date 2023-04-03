fef495b939181fe90f99fd4115c310af62e11a7f
bouhvli <bouhali.hamza.2@gmail.com>
2023-04-03 08:52:00 -0700

    pushing tasks 6 and 7

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,7 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "lists.h"
-
+/**
+ * pop_listint - a function that deletes the head node of a listint_t
+ * linked list.
+ * @head: the list.
+ * Return: returns the head node’s data (n)
+ */
 int pop_listint(listint_t **head)
 {
 	listint_t *current;
--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,6 +1,12 @@
 #include <stdlib.h>
+#include <stddef.h>
 #include <stdio.h>
 #include "lists.h"
+/**
+ * length_list - to get the length of the list
+ * @h: the list.
+ * Return: the number of elements in the list
+ */
 unsigned int length_list(listint_t *head)
 {
 	listint_t *current;
@@ -12,9 +18,16 @@ unsigned int length_list(listint_t *head)
 		current = current->next;
 		i++;
 	}
-	return (i);
+	return (i - 1);
 }
-
+/**
+ * get_nodeint_at_index - a function that returns the nth node of a
+ * listint_t linked list.
+ * @head: the list .
+ * @index: the nth node we wanted.
+ * Return:  the nth node of a listint_t if the node does not exist,
+ * return NULL
+ */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int len, i = 0;
@@ -23,7 +36,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	if (head == NULL)
 		return (NULL);
 	len = length_list(current);
-	if ((len - 1) > index)
+	if (len  > index)
 		return (NULL);
 	else
 	{
