c2371496b5c94f1f63eb79dfac44d33427160dc5
bouhvli <bouhali.hamza.2@gmail.com>
2023-04-03 10:36:57 -0700

    add new the condition

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,6 +15,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 0;
 	listint_t *current, *node = malloc(sizeof(listint_t));
 
+	if (node == NULL)
+		return (NULL);
 	node->n = n;
 	node->next = NULL;
 	if (*head == NULL && idx == 0)
