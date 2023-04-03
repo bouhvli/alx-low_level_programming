6eaf75d24b226c59dd64f0919daf52a481d6fc04
bouhvli <bouhali.hamza.2@gmail.com>
2023-04-03 09:04:07 -0700

    deleting unnecessary function

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,50 +1,29 @@
 #include <stdlib.h>
-#include <stddef.h>
 #include <stdio.h>
 #include "lists.h"
-/**
- * length_list - to get the length of the list
- * @h: the list.
- * Return: the number of elements in the list
- */
-unsigned int length_list(listint_t *head)
-{
-	listint_t *current;
-	unsigned int i = 0;
-	current = head;
-
-	while (current != NULL)
-	{
-		current = current->next;
-		i++;
-	}
-	return (i - 1);
-}
 /**
  * get_nodeint_at_index - a function that returns the nth node of a
  * listint_t linked list.
  * @head: the list .
  * @index: the nth node we wanted.
+ *
  * Return:  the nth node of a listint_t if the node does not exist,
  * return NULL
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int len, i = 0;
-	listint_t *current = head;
+	unsigned int i = 0;
+	listint_t *current;
 
 	if (head == NULL)
 		return (NULL);
-	len = length_list(current);
-	if (len  > index)
-		return (NULL);
-	else
+	current = head;
+	while (i < index)
 	{
-		while (current != NULL && i != index)
-		{
-			current = current->next;
-			i++;
-		}
-		return (current);
+		if (current == NULL)
+			return (NULL);
+		current = current->next;
+		i++;
 	}
+	return (current);
 }
