8d1931aae29cb4b85d213029340388f19de019fd
bouhvli <bouhali.hamza.2@gmail.com>
2023-04-03 08:28:01 -0700

    add checker for the emptyness of the list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -4,12 +4,15 @@
 /**
  * free_listint2 - a function that frees a listint_t list,
  * sets the head to NULL
+ * @head: the list.
  */
 void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 	listint_t *current;
-
+	
+	if (*head == NULL)
+		return;
 	current = *head;
 	while (current != NULL)
 	{
