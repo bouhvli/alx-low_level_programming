0602b024bec9d887b250fdb230be253faca9adc4
bouhvli <bouhali.hamza.2@gmail.com>
2023-04-04 08:17:36 -0700

    push new v

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,34 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
-
-#define TABLE_SIZE 1024
-
 /**
- * print_listint_safe - a function that prints a listint_t linked list.
- * @head: the list
+ * print_listint_safe - prints a listint_t linked list.
+ * @head: pointer to the head of the list
  * Return: the number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *current = head;
+	const listint_t *tmp;
 	size_t count = 0;
-	const listint_t *visited[TABLE_SIZE] = { NULL };
-	size_t hash_idx;
 
-	while (current != NULL)
+	while (head != NULL)
 	{
-		hash_idx = ((size_t)current % TABLE_SIZE);
-		if (visited[hash_idx] == current)
+		printf("[%p] %d\n", (void *)head, head->n);
+		count++;
+		tmp = head;
+		head = head->next;
+		if (tmp <= head)
 		{
-			printf("-> [%p] %d\n", (void *)current, current->n);
+			printf("-> [%p] %d\n", (void *)head, head->n);
 			exit(98);
 		}
-		visited[hash_idx] = current;
-		printf("[%p] %d\n", (void *)current, current->n);
-		count++;
-		current = current->next;
 	}
 	return (count);
 }
-
