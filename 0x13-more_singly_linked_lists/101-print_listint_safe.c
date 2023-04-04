201b4ea40aca6832d01496f53310b7ea23244e44
bouhvli <bouhali.hamza.2@gmail.com>
2023-04-04 06:35:14 -0700

    using hash table to solve this problem

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
+#define MAX 1024
 /**
  * print_listint_safe - a function that prints a listint_t linked list.
  * @head: the list we want to print.
@@ -8,21 +9,23 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *tmp;
-	size_t i = 0;
+	const listint_t *current = head;
+	const listint_t *visited[MAX] = {NULL};
+	size_t i = 0, count = 0;
 
-	while (head != NULL)
+	while (current != NULL)
 	{
-		printf("[%p] %d\n", (void *)head, head->n);
-		i++;
-
-		tmp = head;
-		head = head->next;
-		if (tmp <= head)
+		i = ((size_t)current % MAX);
+		if (visited[i] == current)
 		{
-			printf("-> [%p] %d\n", (void *)head, head->n);
+			printf("-> [%p] %d\n", (void *)current, current->n);
 			exit(98);
 		}
+		visited[i] = current;
+		printf("[%p] %d\n", (void *)current, current->n);
+		count++;
+		current = current->next;
 	}
-	return (i);
+
+	return (count);
 }
