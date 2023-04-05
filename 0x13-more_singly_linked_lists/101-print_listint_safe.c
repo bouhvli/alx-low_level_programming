97891c31d1ecf3c8dc8f5803c261fe9092800d6d
bouhvli <bouhali.hamza.2@gmail.com>
2023-04-05 03:09:04 -0700

    update the function reelaying on the find_loop function cuz the first one misses one node from the list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,18 +8,26 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *tmp;
+	const listint_t *fst_pt = head, *slw_pt = head;
 	size_t count = 0;
 
-	while (head != NULL)
+	while (fst_pt != NULL && fst_pt->next != NULL)
 	{
-		printf("[%p] %d\n", (void *)head, head->n);
+		printf("[%p] %d\n", (void *)slw_pt, slw_pt->n);
 		count++;
-		tmp = head;
-		head = head->next;
-		if (tmp <= head)
+		if (fst_pt->next->next == NULL)
+			return (count);
+		slw_pt = slw_pt->next;
+		fst_pt = fst_pt->next->next;
+		if (slw_pt == fst_pt)
 		{
-
+			while (head != slw_pt)
+			{
+				head = head->next;
+				printf("[%p] %d\n", (void *)slw_pt, slw_pt->n);
+				slw_pt = slw_pt->next;
+				count++;
+			}
 			printf("-> [%p] %d\n", (void *)head, head->n);
 			return (count);
 			exit(98);
