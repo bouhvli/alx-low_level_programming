df20ce4cbfd4d55b8f74782adafac3384e972497
bouhvli <bouhali.hamza.2@gmail.com>
2023-04-05 01:37:22 -0700

    update the function

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -20,8 +20,8 @@ size_t print_listint_safe(const listint_t *head)
 		if (tmp <= head)
 		{
 			printf("-> [%p] %d\n", (void *)head, head->n);
-			return (98);
-			/*exit(98);*/
+			return (count);
+			exit(98);
 		}
 	}
 	return (count);
