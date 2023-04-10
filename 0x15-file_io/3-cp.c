c87ed364dcc2c14e0fff38d7b919fd1e78c17a85
bouhvli <bouhali.hamza.2@gmail.com>
2023-04-10 15:50:06 -0700

    update the permissions

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,9 +1,10 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdarg.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <unistd.h>
-void _close(int n, ...);
+void _close(int n);
 void read_and_write(const char *filename,
 		const char *filename_to, size_t letters);
 void _opened(int f1, int f2, const char *fn1, const char *fn2);
@@ -41,7 +42,7 @@ int main(int ac, char **av)
 {
 	if (ac != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	read_and_write(av[1], av[2], 1024);
@@ -64,23 +65,25 @@ void read_and_write(const char *filename,
 	buffer = malloc(sizeof(char) * letters);
 	if (filename == NULL)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", filename);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+				filename);
 		exit(98);
 	}
 	if (filename_to == NULL)
 	{
-		dprintf(2, "Error: Can't write to %s\n", filename_to);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n",
+				filename_to);
 		exit(99);
 	}
 	file = open(filename, O_RDONLY);
 	_opened(file, 1, filename, "nope");
-	file_to = open(filename_to, O_WRONLY | O_TRUNC | O_CREAT, 662);
+	file_to = open(filename_to, O_WRONLY | O_TRUNC | O_CREAT, 0664);
 	_opened(file, file_to, filename, filename_to);
 	do {
 		bytesRead = read(file, buffer, letters);
 		if (bytesRead < 0)
 		{
-			dprintf(2,
+			dprintf(STDERR_FILENO,
 				"Error: Can't read from file %s\n",
 				filename);
 			exit(98);
@@ -88,36 +91,25 @@ void read_and_write(const char *filename,
 		byteWritten = write(file_to, buffer, bytesRead);
 		if (byteWritten < 0)
 		{
-			dprintf(2,
+			dprintf(STDERR_FILENO,
 				"Error: Can't write to %s\n",
 				filename_to);
 			exit(99);
 		}
 	} while (bytesRead > 0);
-	_close(2, file, file_to);
+	free(buffer), _close(file), _close(file_to);
 }
 /**
  * _close - close files.
- * @n: number of files.
+ * @n: the file .
  */
-void _close(int n, ...)
+void _close(int n)
 {
-	va_list fp;
-	int file, state = -1;
-
-	va_start(fp, n);
-	while (n != 0)
+	if (close(n) < 0)
 	{
-		file = va_arg(fp, int);
-		state = close(file);
-		if (state < 0)
-		{
-			dprintf(2, "Error: Can't close fd %d\n", file);
-			exit(100);
-		}
-		n--;
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", n);
+		exit(100);
 	}
-	va_end(fp);
 }
 /**
  * _opened - check if files are open.
@@ -130,13 +122,14 @@ void _opened(int f1, int f2, const char *fn1, const char *fn2)
 {
 	if (f1 < 0)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", fn1);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+				fn1);
 		exit(98);
 	}
 	if (f2 < 0)
 	{
-		dprintf(2, "Error: Can't write to %s\n", fn2);
-		_close(1, f1);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n",
+				fn2);
 		exit(99);
 	}
 }
