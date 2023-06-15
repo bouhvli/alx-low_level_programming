fcc0747aeaf10a5bad4b6a1a2d2698973c7b241f
bouhvli <bouhali.hamza.2@gmail.com>
2023-06-15 18:46:09 +0100

    pushing the last peace

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -21,17 +21,25 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (add_dnodeint(h, n));
 	for (count = 0; tmp != NULL && count < (idx - 1); count++)
 		tmp = tmp->next;
-	if (tmp == NULL)
+	if (tmp != NULL)
+	{
+		while (tmp->prev != NULL)
+			tmp = tmp->prev;
+	}
+	else if (tmp == NULL)
 		return (NULL);
 	if (tmp->next == NULL)
 		return (add_dnodeint_end(h, n));
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-		return (NULL);
-	new_node->n = n;
-	new_node->prev = tmp;
-	new_node->next = tmp->next;
-	tmp->next->prev = new_node;
-	tmp->next = new_node;
+	else
+	{
+		new_node = malloc(sizeof(dlistint_t));
+		if (new_node == NULL)
+			return (NULL);
+		new_node->n = n;
+		new_node->prev = tmp;
+		new_node->next = tmp->next;
+		tmp->next->prev = new_node;
+		tmp->next = new_node;
+	}
 	return (new_node);
 }
