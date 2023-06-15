79d87a9cf5122eb6c74e09473fa95b352f8285b0
bouhvli <bouhali.hamza.2@gmail.com>
2023-06-15 17:57:55 +0100

    update the condition

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -41,7 +41,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	else if (idx != 0)
 	{
-		while (tmp->next != NULL && i < idx - 1)
+		while (i < idx - 1)
 		{
 			tmp = tmp->next;
 			i++;
