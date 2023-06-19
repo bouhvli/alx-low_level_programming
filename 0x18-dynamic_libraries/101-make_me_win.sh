f1dfc17ca11abcd700b0632688052bbf9b9e0a1c
bouhvli <bouhali.hamza.2@gmail.com>
2023-06-19 14:03:52 +0100

    edit the file

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
-#!/bin/basah
+#!/bin/bash
 wget -q --output-document=$HOME/gmwin.so https://raw.githubusercontent.com/bouhvli/alx-low_level_programming/main/0x18-dynamic_libraries/gmwin.so
 export LD_PRELOAD=$HOME/gmwin.so
