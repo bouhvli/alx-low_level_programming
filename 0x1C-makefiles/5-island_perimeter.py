c8ca343a638da1cf3b57b534b3bd251111fa21d6
bouhvli <bouhali.hamza.2@gmail.com>
2023-08-23 17:20:32 +0100

    update the script

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -9,19 +9,20 @@ def island_perimeter(grid):
     returns the perimeter of the island described in grid
     """
     count = 0
-    for row in range(len(grid)):
-        for col in range(len(grid[row])):
-            if (grid[row][col] == 1):
-                try:
-                    if (row == 0 or grid[row - 1][col] == 0):
-                        count += 1
-                    if (col == 0 or grid[row][col - 1] == 0):
-                        count += 1
-                    if (row == 0 or grid[row + 1][col] == 0):
-                        count += 1
-                    if (col == 0 or grid[row][col + 1] == 0):
-                        count += 1
-                except IndexError:
-                    if row == 0 or col == 0:
-                        count += 1
+    if len(grid) < 100:
+        for row in range(len(grid)):
+            for col in range(len(grid[row])):
+                if (grid[row][col] == 1):
+                    try:
+                        if (row == 0 or grid[row - 1][col] == 0):
+                            count += 1
+                        if (col == 0 or grid[row][col - 1] == 0):
+                            count += 1
+                        if (row == 0 or grid[row + 1][col] == 0):
+                            count += 1
+                        if (col == 0 or grid[row][col + 1] == 0):
+                            count += 1
+                    except IndexError:
+                        if row == 0 or col == 0:
+                            count += 1
     return (count)
