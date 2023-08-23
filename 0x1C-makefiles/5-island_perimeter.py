a5b92b228e1e1a7e8cb12da0d1abc552d1d85df4
bouhvli <bouhali.hamza.2@gmail.com>
2023-08-23 17:04:34 +0100

    adding task number 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+this modul has the solution of the island perimeter
+"""
+
+
+def island_perimeter(grid):
+    """
+    returns the perimeter of the island described in grid
+    """
+    count = 0
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if (grid[row][col] == 1):
+                if (row == 0 or grid[row - 1][col] == 0):
+                    count += 1
+                if (col == 0 or grid[row][col - 1] == 0):
+                    count += 1
+                if (row == 0 or grid[row + 1][col] == 0):
+                    count += 1
+                if (col == 0 or grid[row][col + 1] == 0):
+                    count += 1
+    return (count)
